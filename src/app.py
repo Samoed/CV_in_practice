1967c875bcb2158e390d4585a5911b5f54a2bb88
VorkhlikAS <alevork@gmail.com>
2023-11-22 13:31:58 +0300

    Added image recognition, but it weird right now

--- src/app.py
+++ src/app.py
@@ -6,18 +6,25 @@ from PIL import Image
 # import imageAI
 # import opencv-python
 # import Pillow
-
 # Load the YOLO model
-import os
-
-model = YOLO('models/best.pt')  # Make sure this is the correct path to your model
+model = YOLO('../models/best.pt')  # Make sure this is the correct path to your model
 
 def predict(img):
     # Convert Gradio Image component to OpenCV format
     frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
+    iter = 0
     # Perform inference
     results = model(frame)
+    print(results)
+    print(results[0])
+    for r in results:
+        im_array = r.plot()  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        im.show()  # show image
+        im.save(f'results{iter}.jpg') 
+        iter+=1
+        return im
+    # print(results.boxes[0])
 
     # Print intermediate results for debugging
     # print("Results:", results)
@@ -28,14 +35,14 @@ def predict(img):
 
     # Convert the list of detected signs to a formatted string
     # result_text = "\n".join(detected_signs)
-    return str(results)
+    # return str(results)
 
 # Define the Gradio Interface
 iface = gr.Interface(
     fn=predict,
     # inputs=gr.Image(),  # Use Image component for input
     inputs=gr.Image(sources=["webcam"], streaming=True),  # Use Image component for input
-    outputs="text",     # Output the detected signs as text
+    outputs="image",     # Output the detected signs as text
     live=True            # Enable live updates for the webcam stream
 )
 
