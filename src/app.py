02365f77cce03264e4b0ef0ba456f7b0bf57eeae
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2023-11-23 00:38:45 +0300

    webcam upd

--- src/app.py
+++ src/app.py
@@ -1,50 +1,39 @@
 import gradio as gr
-import cv2
+import pandas as pd
 from ultralytics import YOLO
-from PIL import Image
 
-# import imageAI
-# import opencv-python
-# import Pillow
-# Load the YOLO model
-model = YOLO('../models/best.pt')  # Make sure this is the correct path to your model
+model = YOLO("models/best.pt")  # Make sure this is the correct path to your model
+
 
 def predict(img):
-    # Convert Gradio Image component to OpenCV format
-    frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    iter = 0
-    # Perform inference
-    results = model(frame)
-    print(results)
-    print(results[0])
-    for r in results:
-        im_array = r.plot()  # plot a BGR numpy array of predictions
-        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-        im.show()  # show image
-        im.save(f'results{iter}.jpg') 
-        iter+=1
-        return im
-    # print(results.boxes[0])
+    results = model(img)[0]
+    pred = {"scores": [], "classes": []}
+    for box in results.boxes:
+        pred["scores"].append(box.conf[0].cpu().item())
+        pred["classes"].append(model.names[box.cls[0].cpu().item()])
+    return pd.DataFrame(pred)
 
-    # Print intermediate results for debugging
-    # print("Results:", results)
 
-    # Extract relevant information from the results (modify this based on your model's output)
-    # For demonstration purposes, let's assume the model returns a list of detected traffic signs
-    # detected_signs = [result[0] for result in results.xyxy[0].tolist()]
+def webcam_interface() -> gr.Blocks:
+    with gr.Blocks() as demo:
+        with gr.Row():
+            image = gr.Image(sources=["webcam"], label="Input")
 
-    # Convert the list of detected signs to a formatted string
-    # result_text = "\n".join(detected_signs)
-    # return str(results)
+            table = gr.DataFrame()
+        image.stream(predict, image, table)
+        # image.upload(predict, image, table)
+    return demo
 
-# Define the Gradio Interface
-iface = gr.Interface(
-    fn=predict,
-    # inputs=gr.Image(),  # Use Image component for input
-    inputs=gr.Image(sources=["webcam"], streaming=True),  # Use Image component for input
-    outputs="image",     # Output the detected signs as text
-    live=True            # Enable live updates for the webcam stream
-)
 
-# Launch the Gradio app
-iface.launch()
+def video_result(img):
+    results = model(img)[0]
+    print(results.render())
+    return results.render()
+
+
+def video_interface() -> gr.Interface:
+    return gr.Interface(fn=predict, inputs="video", outputs="video")
+
+
+webcam_interface().queue().launch()
+# video_interface().launch()
